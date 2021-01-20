2594a256dd024ec94bad02712c3ee8a2004393e3
Abraham Lincoln <svinogr@hotmail.com>
2021-01-20 16:16:34 +0300

    release

--- TaskTwo.go
+++ TaskTwo.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Task No.2")
+}
+
+func main() {
+	http.HandleFunc("/", mainPage)
+	http.ListenAndServe(":8080", nil)
+}
