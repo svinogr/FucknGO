89cfbb668beb451779f3b44a3a21d27cd3f0ad6b
svinogr <svinogr@hotmail.com>
2021-01-21 20:46:43 +0300

    add goroutine

--- app/server/Server.go
+++ app/server/Server.go
@@ -11,16 +11,22 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./app/server/ui/static/"))
+	fileServer := http.FileServer(http.Dir("./app/server/ui/static")) // ./app/server/ui/static  if start SERVER from root project
 
 	http.HandleFunc("/", mainPage)
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	err := http.ListenAndServe(":8080", nil)
+	go startServer()
+
+	 fmt.Println("Successfully started server on http://localhost:8080")
+
+	select {}
+}
+
+func startServer() {
+	err := http.ListenAndServe(":8080", nil )
 
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error while starting serverErrors: %v", err)
 	}
-
-	fmt.Println("for start go to:  127.0.0.1:8080/")
 }
