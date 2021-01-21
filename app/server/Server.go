716a0c2f4a2e9c90a661fe215f7ec3fcd7ef1829
Сергей <!@>
2021-01-21 16:48:22 +0300

    fix message for user

--- app/server/Server.go
+++ app/server/Server.go
@@ -2,24 +2,25 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Task No.2")
+	fmt.Fprintf(w, "Главная")
 }
 
 func main() {
-	fmt.Println("for start go to:  127.0.0.0:8080/")
-
-	fileServer := http.FileServer(http.Dir("./app/ui/static/"))
+	fileServer := http.FileServer(http.Dir(".app/server/ui/static/"))
 
 	http.HandleFunc("/", mainPage)
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	err := http.ListenAndServe(":80w80", nil)
+	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
+
+	fmt.Println("for start go to:  127.0.0.1:8080/")
 }
