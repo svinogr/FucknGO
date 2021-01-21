da797b48a1194c82150312e8a692169b793b70ad
Сергей <!@>
2021-01-21 16:49:54 +0300

    fix adress file server

--- app/server/Server.go
+++ app/server/Server.go
@@ -11,7 +11,7 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir(".app/server/ui/static/"))
+	fileServer := http.FileServer(http.Dir("./app/server/ui/static/"))
 
 	http.HandleFunc("/", mainPage)
 	http.Handle("/static/", http.StripPrefix("/static", fileServer))
