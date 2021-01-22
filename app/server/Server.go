dd730c414197d4f5b271bc5a72fb67c8af2818a2
Сергей <!@>
2021-01-22 11:18:20 +0300

    add chek for static folder and create it if not exist

--- app/server/Server.go
+++ app/server/Server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func mainPage(w http.ResponseWriter, r *http.Request) {
@@ -11,20 +12,33 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./app/server/ui/static")) // ./app/server/ui/static  if start SERVER from root project
-
 	http.HandleFunc("/", mainPage)
-	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	setupStaticResource()
 
 	go startServer()
 
-	 fmt.Println("Successfully started server on http://localhost:8080")
+	fmt.Println("Successfully started server on http://localhost:8080")
 
 	select {}
 }
 
+func setupStaticResource() {
+	path := "./app/server/ui/static"
+
+	_, err := os.Stat(path)
+
+	if err != nil {
+		os.MkdirAll(path, 0777)
+	}
+
+	fileServer := http.FileServer(http.Dir(path))
+
+	http.Handle("/static/", http.StripPrefix("/static", fileServer))
+}
+
 func startServer() {
-	err := http.ListenAndServe(":8080", nil )
+	err := http.ListenAndServe(":8080", nil)
 
 	if err != nil {
 		log.Fatalf("Error while starting serverErrors: %v", err)
