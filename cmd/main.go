111453e5c063e60d62f4c28b14504dce700e2341
svinogr <svinogr@hotmail.com>
2021-01-25 23:47:57 +0300

    to Alex

--- cmd/main.go
+++ cmd/main.go
@@ -1,36 +1,8 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"os"
-)
-
-type Config struct {
-	Port int8
-}
+import "FucknGO/config"
 
 func main() {
-	fileJson, err := os.Open("./config/config.json")
-
-	if err != nil {
-		fmt.Print(err)
-		os.Exit(1)
-	}
-
-	defer fileJson.Close()
-
-	dataJson := make([]byte, 64)
-
-	_, err = fileJson.Read(dataJson)
-
-	fmt.Println(dataJson)
-
-	var jsonConfig map[string] string
-
-	json.Unmarshal(dataJson, jsonConfig)
-
-	fmt.Print(jsonConfig["port"]) // вот тут нил
 
 
 }
